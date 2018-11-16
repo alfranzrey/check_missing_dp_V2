4bb9a16c209b9220957d8d596a0804ffe7735ed2
localuser <localuser@CBPO004.CBPOCEBU.com>
2018-11-17 00:15:02 +0800

    updated

--- check.js
+++ check.js
@@ -30,13 +30,14 @@ function objToString (obj) {
     return str;
 }
 //-------------------------
-
+const page = new Array();
+var browser;
 
 //Main async function
 (async function main() {
     try{
         //---------------
-        const browser = await puppeteer.launch({headless: false});
+        browser = await puppeteer.launch({headless: false});
         
         /*block images and css
             await page.setRequestInterception(true);
@@ -52,48 +53,57 @@ function objToString (obj) {
         //-----------------
 
         //code starts here
-        var paraCount = 2;
-        const page = new Array();
-        for(var x = 0; x < paraCount;x++){
-            page[x] = await browser.newPage();
-            page[x].setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36');
-            gather(x, 0, csvData.length);
+        var paraCount = 5;
+        var curr = 0;
+        for(var x = 0; x < 5;x++){
+            if (curr < paraCount) {
+                gather(x, csvData[x]);   
+            }
+            else{
+                x--;
+                continue;
+            }
+            
         }
 
         //loop gather()
 
         //end
-        console.log("All done!");
+        //console.log("All done!");
         //browser.close();
     }
     catch(err){
         console.log("!!!! >>>>>  my error",err);
     }
 
-    async function gather(paraNo, paraStart, lenght){
-        //console.log("inside gather: " + paraNo +" : "+paraStart+" : "+lenght);
-        for(var i = paraStart; i < lenght; i++){
-            var startT = new Date();
-            await page[paraNo].goto("https://www.amazon.com/gp/offer-listing/"+csvData[i], {waitUntil: 'load', timeout: 0}); //bypass timeout
-            await page[paraNo].waitForSelector('body');
+    async function gather(index, item){
+        //console.log("inside gather: " + paraNo +" : "+paraStart+" : "+length);
+        page[index] = await browser.newPage();
+        curr++;
+        page[index].setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36');
+     
+            //var startT = new Date();
+            await page[index].goto("https://www.amazon.com/gp/offer-listing/"+item, {waitUntil: 'load', timeout: 0}); //bypass timeout
+            await page[index].waitForSelector('body');
             var title = "";
 
-            if (await page[paraNo].$('#olpProductDetails > h1') !== null){
-                title = await page[paraNo].evaluate(() => document.querySelector('#olpProductDetails > h1').innerText); 
+            if (await page[index].$('#olpProductDetails > h1') !== null){
+                title = await page[index].evaluate(() => document.querySelector('#olpProductDetails > h1').innerText); 
             }
             else{
                 title = "Missing Detail page";
             }
             let row = {
-                    'Style':csvData[i],
+                    'Style':item,
                     'Title':title
                 }
             exportToCSV.write(objToString(row) + '\n','utf-8');
             console.log(objToString(row)); 
-            var endT = new Date() - startT;
+            page[index].close();
+            curr--;
+            //var endT = new Date() - startT;
             //console.log("Execution time: "+endT + "ms" + '\n');
-            ETC(endT, csvData.length-i-1);
-            }
+            //ETC(endT, csvData.length-i-1);
         }
 
     function ETC(durationPerLoop, loopsRemaining){
