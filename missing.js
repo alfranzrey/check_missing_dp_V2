ec6108798b2fa96ebd83232d34007a5773136f25
Alfranz Rey Cordovan <arcordovan@channelbpocebu.com>
2018-12-29 07:04:40 +0800

    semi working

--- missing.js
+++ missing.js
@@ -15,7 +15,7 @@ fs.createReadStream('asins.csv')
     });
 //-----------------------
 //-export file result
-var exportToCSV = fs.createWriteStream('result3.txt');
+var exportToCSV = fs.createWriteStream('result.txt');
 var header ='ASIN'  + '\t' +
             'Title'    + '\n';
 console.log(header);
@@ -40,11 +40,7 @@ function objToString (obj) {
         const browser = await puppeteer.launch({executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
             headless: false});
         const page = await browser.newPage();
-        page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');
-        const maxPageLimit = 9;
-        var totalPageCounts = csvData.length;
-        var curPageCount = 0;
-        const links = new Array;
+        page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');    
         /*block images and css
             await page.setRequestInterception(true);
             page.on('request', (req) => {
@@ -58,91 +54,60 @@ function objToString (obj) {
             */
         //-----------------
 
-        //gather all the links
+        //-------------gather all the links
+        var maxTabCount = 9;
+        var curPageCount = 0;
+        var iteration = -1;
+        var subLinkCount = 0;
+        const subLinks = [[]]; //all links
+        //populate all sublinks
         for(var i = 0; i < csvData.length; i++){
-            links[i] = "https://www.amazon.com/gp/offer-listing/"+csvData[i];
-        }//----
-        
-        //async all links
-        await Promise.all(links.map(async(link) =>{
-            var title = "Missing Detail Page";
-            try{
-                const curPage = await browser.newPage();
-                await curPage.goto(link);
-                curPageCount++;
-                console.log("opening tab.. "+curPageCount); 
-                await curPage.waitForSelector('body');
-                if (await curPage.$('#olpProductDetails > h1') !== null){
-                    title = await curPage.evaluate(() => document.querySelector('#olpProductDetails > h1').innerText); 
-                }
-                console.log(link+ " = " +title);
-                curPage.close();
-                curPageCount--;
-                console.log(curPageCount); 
+            iteration++;
+            if(iteration == maxTabCount){
+                subLinkCount++;
+                iteration = 0; 
+                console.log("Sublink count: "+subLinkCount+" Iteration: "+iteration+" > "+csvData[i]);
+                 
             }
-            catch(err){
-                //console.log(link + "> Promise ER: "+err);
-                //title = "Missing Detail Page!";
-                console.log(link+ " = " +title);
-                curPage.close();
-                curPageCount--;
-                console.log(curPageCount); 
-            }
-            
-        }));//-----------
+            subLinks[subLinkCount][iteration]= "https://www.amazon.com/gp/offer-listing/"+csvData[i];
+            console.log(subLinkCount+"-"+iteration+">"+csvData[i]+ " == "+subLinks[subLinkCount][iteration]);
+        }//--------------------
 
-
+        //--------display in mutiple tabs by batch
         /*
-        while(totalPageCounts > 0)
-        {
-            const curPage =await browser.newPage();
-            
-            curPageCount++;
-            console.log(curPageCount+ " = " +csvData[totalPageCounts]);
-
-            if (curPageCount == 7) {
-                await Promise.all(); 
-            }
-        }
-        */
-
-
-        /*for(var i = 0; i < csvData.length; i++){
-        //for(const i of csvData){
-            const curPageCount = 0;
-            const curPage =await browser.newPage();
-            await curPage.goto("https://www.amazon.com/gp/offer-listing/"+csvData[i]);
-            //curPageCount++;
-            
-            //await pageArray[i].waitForSelector('body');
-            console.log(csvData[i]);       
-        }*/
-
-        /*code starts here
-        for(var i = 0; i < csvData.length; i++){
-            var startT = new Date();//for ETC
-            await page.goto("https://www.amazon.com/gp/offer-listing/"+csvData[i], {waitUntil: 'load', timeout: 0}); //bypass timeout
-            await page.waitForSelector('body');
-            var title = "";
-
-            if (await page.$('#olpProductDetails > h1') !== null){
-                title = await page.evaluate(() => document.querySelector('#olpProductDetails > h1').innerText); 
-            }
-            else{
-                title = "Missing Detail page";
-            }
-            let row = {
-                    'ASIN':csvData[i],
-                    'Title':title
+        for(var x = 0; x <= subLinkCount; x++){
+            await Promise.all(subLinks[x].map(async(link) =>{
+                var title = "Missing Detail Page";
+                var currentASIN = link.slice(40,50);
+                try{
+                    const curPage = await browser.newPage();
+                    await curPage.goto(link);
+                    curPageCount++;
+                    //console.log("opening tab.. "+curPageCount); 
+                    //-code starts here
+                    await curPage.waitForSelector('body');
+                    if (await curPage.$('#olpProductDetails > h1') !== null){
+                        title = await curPage.evaluate(() => document.querySelector('#olpProductDetails > h1').innerText); 
+                    }
+                    console.log(currentASIN+": "+title);
+                    curPage.close();
+                    curPageCount--;
+                    //console.log(curPageCount); 
                 }
-            exportToCSV.write(objToString(row) + '\n','utf-8');
-            console.log("No. "+i+ " ");
-            console.log(objToString(row)); 
-            var endT = new Date() - startT; //for ETC
-            ETC(endT, csvData.length-i-1);
-            await delay(750);
-        }*/
-        //end
+                catch(err){
+                    //console.log(link + "> Promise ER: "+err);
+                    //title = "Missing Detail Page!";
+                    console.log(currentASIN+": "+title);
+                    curPage.close();
+                    curPageCount--;
+                    console.log(curPageCount); 
+                }
+            
+            }));//end promise      
+        }//end for
+        */
+        //--------------------------
+
  
         console.log("All done!");
         //browser.close();
