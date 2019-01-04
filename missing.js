dd5089bb3a077747144ac39fd53061e563a71e3f
Alfranz Rey Cordovan <arcordovan@channelbpocebu.com>
2019-01-05 05:27:43 +0800

    updated

--- missing.js
+++ missing.js
@@ -41,7 +41,7 @@ function objToString (obj) {
             headless: false});
         const page = await browser.newPage();
         page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36');    
-        //block images and css
+        /*block images and css
             await page.setRequestInterception(true);
             page.on('request', (req) => {
                 if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image'){
@@ -51,7 +51,7 @@ function objToString (obj) {
                     req.continue();
                 }
             });
-            //
+            */
         //-----------------
 
         //
